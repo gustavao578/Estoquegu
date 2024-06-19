1a17cc624ebcc299a7b8b2004bc0f380452105af
Fabulas1 <fabiomouranascimento4@gmail.com>
2024-06-19 15:28:56 -0300

    Create AppEstoque.java

---
+++ AppEstoque_1/src/appestoque/AppEstoque.java
@@ -0,0 +1,15 @@
+
+package appestoque;
+
+import interfaces.Login;
+import interfaces.Principal;
+
+public class AppEstoque {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login l = new Login();
+        l.setVisible(true);
+    }
+    
+}
